2bb73fa4de09c2e3fc9dc8073fc477b59d6cc257
Ashu Aggarwal <ashu@iogravity.com>
2023-05-03 22:23:53 +0530

    Final Commit

---
+++ Bloggie.Web/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bloggie.Web.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        [MinLength(6, ErrorMessage = "Password has to be at least 6 characters")]
+        public string Password { get; set; }
+
+        public string? ReturnUrl { get; set; }
+    }
+}
